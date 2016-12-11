9a05bf6d6439a7e58c7a6296003a5e41f241ce02
Jonas de A Luz Jr. (Notebook) <git@jonasluz.com>
2016-12-11 11:08:30 -0300

    Trabalho 2 - Identificação das relações entre as pessoas.

---
+++ Part-2.R
@@ -0,0 +1,38 @@
+## Part-2.R 
+#
+# Requer a execução prévia do Part-1.R ou leitura dos dados de ambiente R.
+#
+# ##
+
+#setwd("C:\\Users\\jonas\\JONAS\\MIA\\mia-pln")
+
+# Prepara a lista de documentos para tratamento.
+#
+# Retira identificação de processos.
+docs.clear <- sub(docs.text, pattern = "Proc: P([0-9]{6})/([0-9]{4})", replacement = "", perl=TRUE)
+# Converte para caixa baixa.
+docs.lower <- stri_trans_tolower(docs.clear)
+# Corrige pontuação, em função de se ter detectado erros crassos neste aspecto.
+docs.lower <- gsub(docs.lower, pattern = "[.]", replacement = ". ", perl = TRUE, fixed = FALSE)
+docs.lower <- gsub(docs.lower, pattern = "[,]", replacement = ", ", perl = TRUE, fixed = FALSE)
+docs.lower <- gsub(docs.lower, pattern = "sr:", replacement = "sr. ", perl = TRUE)
+docs.lower <- gsub(docs.lower, pattern = "sra:", replacement = "sra. ", perl = TRUE)
+# Retira espaços extras.
+docs.lower <- gsub(trimws(docs.lower), pattern = "\\s+", replacement = " ")
+
+# Carrega ligação e funções de uso da Rosette API.
+source(".\\Rosette.R", encoding = "UTF-8")
+# Carrega funções de busca por relações entre as pessoas.
+source(".\\Relatives.R", encoding = "UTF-8")
+
+# PROCESSAMENTO DO CORPUS COM EXTRAÇÃO DAS ENTIDADES NOMEADAS 
+# E RELAÇÕES ENTRE AS PESSOAS.
+# 
+library(foreach)
+length <- length(docs.lower)
+result <- foreach(i = 1 : length) %do% 
+  unlist(list( EntitiesFrom(docs.lower[[i]], index = i),
+         list( relations = FindRelations(docs.lower[[i]], index = i)$relations)
+            ), recursive = FALSE
+         )
+# ##
---
+++ Relatives.R
@@ -0,0 +1,42 @@
+## Relatives.R
+# 
+# Identifica relações de parentesco.
+#
+# ##
+
+library(stringi)
+
+## Função para encontrar relações.
+# 
+FindRelations <- function(x, index = NULL) 
+{
+  ## Relações...
+  # 
+  relations <- c("pai", "mãe", "irmão", "irmã", "primo", "prima", "tio", "tia", 
+                 "neto", "neta", "bisneto", "bisneta", "cunhado", "cunhada", 
+                 "marido", "esposo", "mulher", "esposa", "noivo", "noiva", 
+                 "namorado", "namorada", "avô", "avó", "bisavô", "bisavó", 
+                 "sobrinho", "sobrinha", "sogro", "sogra", "genro", "nora",
+                 "enteado", "enteada", "entiado", "entiada", # (SIC)
+                 "companheiro", "companheira", "parceiro", "parceira",
+                 "compadre", "comadre", "cumpadre", "cumadre", #(SIC)
+                 "amigo", "amiga", "vizinho", "vizinha", "ex", #, "si"
+                 "filho", "filha"
+  )
+  
+  # Faz a análise morfológica, encontrando tokens e posTags.
+  #
+  result <- PartsOfSpeech(x, index)
+  if (result$origin == "") return(result)
+  
+  # Procura por relações de família na lista de tokens.
+  #
+  indexes <- which(result$tokens %in% relations, arr.ind = TRUE)
+  result$relations <- result$tokens[indexes]
+  
+  # Limpa o POS do resultado antes de retornar.
+  result$tokens <- NULL
+  result$posTags <- NULL
+  
+  return(result)
+}
